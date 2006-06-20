b44888e64d120331cabad38db3b69cc4dabc24c8
Chris Mottram <C.Mottram@ljmu.ac.uk>
2006-06-20 13:10:38 +0000

    Added Autoguider_CIL_Guide_Packet_Send_Set/Get.

--- include/autoguider_cil.h
+++ include/autoguider_cil.h
@@ -1,5 +1,5 @@
 /* autoguider_cil.h
-** $Header: /home/cjm/cvs/autoguider/include/autoguider_cil.h,v 1.1 2006-06-12 19:21:13 cjm Exp $
+** $Header: /home/cjm/cvs/autoguider/include/autoguider_cil.h,v 1.2 2006-06-20 13:10:38 cjm Exp $
 */
 #ifndef AUTOGUIDER_CIL_H
 #define AUTOGUIDER_CIL_H
@@ -12,8 +12,13 @@ extern int Autoguider_CIL_Guide_Packet_Open(void);
 extern int Autoguider_CIL_Guide_Packet_Send(float x_pos,float y_pos,int terminating,int unreliable,float timecode_secs,
 				     char status_char);
 extern int Autoguider_CIL_Guide_Packet_Close(void);
+extern int Autoguider_CIL_Guide_Packet_Send_Set(int on);
+extern int Autoguider_CIL_Guide_Packet_Send_Get(void);
 
 /*
 ** $Log: not supported by cvs2svn $
+** Revision 1.1  2006/06/12 19:21:13  cjm
+** Initial revision
+**
 */
 #endif
