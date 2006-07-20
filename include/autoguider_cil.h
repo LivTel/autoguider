7564cdacfeea622fff0fcb54677fc2d004cf796e
Chris Mottram <C.Mottram@ljmu.ac.uk>
2006-07-20 15:12:02 +0000

    Added CIL submission software.

--- include/autoguider_cil.h
+++ include/autoguider_cil.h
@@ -1,8 +1,10 @@
 /* autoguider_cil.h
-** $Header: /home/cjm/cvs/autoguider/include/autoguider_cil.h,v 1.2 2006-06-20 13:10:38 cjm Exp $
+** $Header: /home/cjm/cvs/autoguider/include/autoguider_cil.h,v 1.3 2006-07-20 15:12:02 cjm Exp $
 */
 #ifndef AUTOGUIDER_CIL_H
 #define AUTOGUIDER_CIL_H
+/* for eAgsState_t etc... */
+#include "ngatcil_ags_sdb.h"
 
 /* external functions */
 extern int Autoguider_CIL_Server_Initialise(void);
@@ -15,8 +17,20 @@ extern int Autoguider_CIL_Guide_Packet_Close(void);
 extern int Autoguider_CIL_Guide_Packet_Send_Set(int on);
 extern int Autoguider_CIL_Guide_Packet_Send_Get(void);
 
+extern int Autoguider_CIL_SDB_Packet_Open(void);
+extern int Autoguider_CIL_SDB_Packet_State_Set(eAgsState_t state);
+extern int Autoguider_CIL_SDB_Packet_Exp_Time_Set(int ms);
+extern int Autoguider_CIL_SDB_Packet_Centroid_Set(float cx,float cy,float fwhm,float mag);
+extern int Autoguider_CIL_SDB_Packet_Send(void);
+extern int Autoguider_CIL_SDB_Packet_Close(void);
+extern int Autoguider_CIL_SDB_Packet_Send_Set(int on);
+extern int Autoguider_CIL_SDB_Packet_Send_Get(void);
+
 /*
 ** $Log: not supported by cvs2svn $
+** Revision 1.2  2006/06/20 13:10:38  cjm
+** Added Autoguider_CIL_Guide_Packet_Send_Set/Get.
+**
 ** Revision 1.1  2006/06/12 19:21:13  cjm
 ** Initial revision
 **
