719e61319758e03ccf3231b0d1438883272d4198
Chris Mottram <C.Mottram@ljmu.ac.uk>
2006-08-29 14:07:04 +0000

    Added Agstate command.

--- include/autoguider_command.h
+++ include/autoguider_command.h
@@ -1,5 +1,5 @@
 /* autoguider_command.h
-** $Header: /home/cjm/cvs/autoguider/include/autoguider_command.h,v 1.4 2006-06-27 20:44:43 cjm Exp $
+** $Header: /home/cjm/cvs/autoguider/include/autoguider_command.h,v 1.5 2006-08-29 14:07:04 cjm Exp $
 */
 #ifndef AUTOGUIDER_COMMAND_H
 #define AUTOGUIDER_COMMAND_H
@@ -21,6 +21,7 @@ enum COMMAND_AG_ON_TYPE
 };
 
 extern int Autoguider_Command_Abort(char *command_string,char **reply_string);
+extern int Autoguider_Command_Agstate(char *command_string,char **reply_string);
 extern int Autoguider_Command_Autoguide(char *command_string,char **reply_string);
 extern int Autoguider_Command_Autoguide_On(enum COMMAND_AG_ON_TYPE on_type,float pixel_x,float pixel_y,int rank);
 extern int Autoguider_Command_Config_Load(char *command_string,char **reply_string);
@@ -34,6 +35,10 @@ extern int Autoguider_Command_Log_Level(char *command_string,char **reply_string
 
 /*
 ** $Log: not supported by cvs2svn $
+** Revision 1.4  2006/06/27 20:44:43  cjm
+** Added Autoguider_Command_Autoguide_On.
+** Added COMMAND_AG_ON_TYPE enum.
+**
 ** Revision 1.3  2006/06/20 13:10:38  cjm
 ** Added Autoguider_Command_Autoguide.
 **
