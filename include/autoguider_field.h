dbfcd5f1ea799f362782ca76732c402409c280a2
Chris Mottram <C.Mottram@ljmu.ac.uk>
2006-11-14 18:10:40 +0000

    Added Autoguider_Field_In_Object_Bounds.

--- include/autoguider_field.h
+++ include/autoguider_field.h
@@ -1,5 +1,5 @@
 /* autoguider_field.h
-** $Header: /home/cjm/cvs/autoguider/include/autoguider_field.h,v 1.2 2006-06-20 13:10:38 cjm Exp $
+** $Header: /home/cjm/cvs/autoguider/include/autoguider_field.h,v 1.3 2006-11-14 18:10:40 cjm Exp $
 */
 #ifndef AUTOGUIDER_FIELD_H
 #define AUTOGUIDER_FIELD_H
@@ -16,9 +16,13 @@ extern int Autoguider_Field_Set_Do_Flat_Field(int doit);
 extern int Autoguider_Field_Get_Do_Flat_Field(void);
 extern int Autoguider_Field_Set_Do_Object_Detect(int doit);
 extern int Autoguider_Field_Get_Do_Object_Detect(void);
+extern int Autoguider_Field_In_Object_Bounds(float ccd_x,float ccd_y);
 
 /*
 ** $Log: not supported by cvs2svn $
+** Revision 1.2  2006/06/20 13:10:38  cjm
+** Added locking parameter to Autoguider_Field_Exposure_Length_Set.
+**
 ** Revision 1.1  2006/06/01 15:19:05  cjm
 ** Initial revision
 **
