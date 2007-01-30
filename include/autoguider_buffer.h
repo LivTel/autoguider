ec7a565cef0498d5e12987e3d9d48cf3bb664860
Chris Mottram <C.Mottram@ljmu.ac.uk>
2007-01-30 17:41:44 +0000

    Added utoguider_Buffer_Field/Guide_CCD_Temperature_Set/Get.

--- include/autoguider_buffer.h
+++ include/autoguider_buffer.h
@@ -1,5 +1,5 @@
 /* autoguider_buffer.h
-** $Header: /home/cjm/cvs/autoguider/include/autoguider_buffer.h,v 1.2 2007-01-26 18:03:46 cjm Exp $
+** $Header: /home/cjm/cvs/autoguider/include/autoguider_buffer.h,v 1.3 2007-01-30 17:41:43 cjm Exp $
 */
 #ifndef AUTOGUIDER_BUFFER_H
 #define AUTOGUIDER_BUFFER_H
@@ -35,15 +35,23 @@ extern int Autoguider_Buffer_Field_Exposure_Start_Time_Set(int index,struct time
 extern int Autoguider_Buffer_Field_Exposure_Start_Time_Get(int index,struct timespec *start_time);
 extern int Autoguider_Buffer_Field_Exposure_Length_Set(int index,int exposure_length_ms);
 extern int Autoguider_Buffer_Field_Exposure_Length_Get(int index,int *exposure_length_ms);
+extern int Autoguider_Buffer_Field_CCD_Temperature_Set(int index,double current_ccd_temperature);
+extern int Autoguider_Buffer_Field_CCD_Temperature_Get(int index,double *current_ccd_temperature);
+
 extern int Autoguider_Buffer_Guide_Exposure_Start_Time_Set(int index,struct timespec start_time);
 extern int Autoguider_Buffer_Guide_Exposure_Start_Time_Get(int index,struct timespec *start_time);
 extern int Autoguider_Buffer_Guide_Exposure_Length_Set(int index,int exposure_length_ms);
 extern int Autoguider_Buffer_Guide_Exposure_Length_Get(int index,int *exposure_length_ms);
+extern int Autoguider_Buffer_Guide_CCD_Temperature_Set(int index,double current_ccd_temperature);
+extern int Autoguider_Buffer_Guide_CCD_Temperature_Get(int index,double *current_ccd_temperature);
 
 extern int Autoguider_Buffer_Shutdown(void);
 
 /*
 ** $Log: not supported by cvs2svn $
+** Revision 1.2  2007/01/26 18:03:46  cjm
+** Field and Guide Exposure Length/Exposure Start Time Getters/Setters.
+**
 ** Revision 1.1  2006/06/01 15:19:05  cjm
 ** Initial revision
 **
